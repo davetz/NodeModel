cd60bb03d4ca2ebd8f5c9ec19cbacc31884a84c0
David Tetzlaff <davetz@outlook.com>
2019-05-08 17:38:06 -0500

    Begin roughing out metadata view on NodeModelTestApp

---
+++ NodeModel/NodeModel/Adapters/Interface/IMetadataDraw.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Numerics;
+
+using Windows.Foundation;
+
+namespace NodeModel
+{
+    /// <summary>
+    /// Win2D wants to draw everything in one Invalidate event
+    /// </summary>
+    public interface IMetadataDraw : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// A notify on property change can be hooked up to Canvas.Invalidate(); 
+        /// </summary>
+        bool Invalidate { get; }
+
+        /// <summary>
+        /// All rectangles on the metadata diagram
+        /// </summary>
+        IList<Rect> MetadataRects { get; }
+ 
+        /// <summary>
+        /// All splines on the metadata diagram
+        /// </summary>
+        IList<(Vector2[] Points, bool IsDotted, byte Width, (byte A, byte R, byte G, byte B) Color)> MetadataSplines { get; }
+
+        /// <summary>
+        /// All text on the metadata diagram
+        /// </summary>
+        IList<(Vector2 TopLeft, string Text, (byte A, byte R, byte G, byte B) Color)> MetadataText { get; }
+    }
+}
--- NodeModel/NodeModelTestApp/Views/MetadataPage.xaml.cs
+++ NodeModel/NodeModelTestApp/Views/MetadataPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
-
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using NodeModelTestApp.Services;
 using NodeModelTestApp.ViewModels;
-
+using Windows.Foundation;
+using Windows.UI;
 using Windows.UI.Xaml.Controls;
 
 namespace NodeModelTestApp.Views
@@ -13,6 +15,38 @@ namespace NodeModelTestApp.Views
         public MetadataPage()
         {
             InitializeComponent();
+            DataContext = NodeModelService.Current.CurentModel;
         }
+
+
+        #region EditorCanvas_Draw  ============================================
+        private void EditorCanvas_Draw(CanvasControl sender, CanvasDrawEventArgs args)
+        {
+            var ds = args.DrawingSession;
+            Rect rect = new Rect(50, 50, 100, 60);
+            var color2 = Color.FromArgb(0xff, 0xff, 0xff, 0x80);
+            ds.DrawRoundedRectangle(rect, 5, 5, color2, 3);
+        }
+
+        #endregion
+
+        #region EditorCanavas_Loaded  =======================================
+        private void EditorCanvas_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            EditorCanvas.Invalidate();
+        }
+
+        #endregion
+
+        #region Page_Unloaded  =============================================
+        private void Page_Unloaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (EditorCanvas != null)
+            {
+                EditorCanvas.RemoveFromVisualTree();
+                EditorCanvas = null;
+            }
+        }
+        #endregion
     }
 }
